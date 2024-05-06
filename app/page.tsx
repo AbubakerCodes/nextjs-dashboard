9d57121360ee12ced031412738932e390cf90b95
AbubakerCodes <drbeekox@gmail.com>
2024-05-06 23:46:37 +0300

    Bug fix: Latest invoices and Revenue chart

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,11 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import {fetchRevenue} from '@/app/lib/data'
-import { fetchLatestInvoices } from '@/app/lib/data';
-import { fetchCardData } from '@/app/lib/data';
+import { fetchLatestInvoices, fetchRevenue,  fetchCardData} from '@/app/lib/data';
  
 export default async function Page() {
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
+  // const revenue = await fetchRevenue();
+  // const latestInvoices = await fetchLatestInvoices();
   const {totalPaidInvoices, totalPendingInvoices,numberOfInvoices, numberOfCustomers} = await fetchCardData();
   return (
     <main>
@@ -23,8 +21,8 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}  />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <RevenueChart   />
+        <LatestInvoices  />
       </div>
     </main>
   );
